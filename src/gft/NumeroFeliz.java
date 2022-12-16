f777813b2ff7e9820510353791c374969ae46607
ju-tilly <juce.duarte.f@gmail.com>
2022-12-16 18:40:27 -0300

    Desafio de Código - Número Feliz

--- src/gft/NumeroFeliz.java
+++ src/gft/NumeroFeliz.java
@@ -1,5 +1,38 @@
 package gft;
 
-public class NumeroFeliz {
+import java.util.Scanner;
 
+public class NumeroFeliz {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        //TODO: Desenvolva um algoritmo que retorne se o número é feliz ou não
+
+        String[] n_string = String.valueOf(n).split("");
+
+        int resultado = 0;
+        int posicao = 0;
+
+        String resultadoString = "";
+
+        do {
+            for (int i = 0; i < n_string.length; i++){
+                posicao = Integer.parseInt(String.valueOf(n_string[i]));
+                resultado = (posicao * posicao) + resultado;
+                resultadoString = String.valueOf(resultado);
+            }
+
+            if(resultado == 1){
+                System.out.println(true);
+                break;
+            }else if(resultadoString.length() == 1){
+                System.out.println(false);
+                break;
+            }
+            n_string = String.valueOf(resultadoString).split("");
+            resultado = 0;
+
+        }while (true);
+    }
 }
